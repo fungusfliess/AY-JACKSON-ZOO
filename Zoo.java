72dfe961c4241862ad517d3bfa8efaf425983954
Jerry Ning <jerry.ning1025@gmail.com>
2026-01-07 10:35:41 -0500

    added zoo class

---
+++ Zoo.java
@@ -0,0 +1,9 @@
+public class Zoo {
+    int numberOfAnimals;
+    public Zoo(int numberOfAnimals) {
+        this.numberOfAnimals = numberOfAnimals;
+    }
+    public String toString() {
+        return "This zoo has " + numberOfAnimals + " animals.";
+    }
+}
--- ZooRunner.java
+++ ZooRunner.java
@@ -1,5 +1,7 @@
 public class ZooRunner {
   public static void main(String[] args) {
       System.out.println("hello bingus");
+      Zoo AYJZoo = new Zoo(150);
+      System.out.println(AYJZoo);
     }
 }
