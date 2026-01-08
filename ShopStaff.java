715b28e702dc339b6953558b638b52fb9ec28686
Elizabeth <ilikebuttons76@gmail.com>
2026-01-08 11:27:17 -0500

    shopstaff edits

---
+++ ShopStaff.java
@@ -0,0 +1,18 @@
+/*
+   File Name: ZooKeeper.java
+   Name: Elizabeth Wang
+   Class: ICS4U1-23
+   Date: Jan 7, 2025
+   Description: 
+*/
+
+public class ShopStaff {
+    
+    public ShopStaff(int age, String personID, String firstName, String lastName, double hourlyWage, int yearsOfExperience){
+
+
+    }
+
+
+    
+}
