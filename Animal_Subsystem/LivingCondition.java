ce11bd2269f011fcd1cde0d34550ae5ad334e60c
Jerry Ning <jerry.ning1025@gmail.com>
2026-01-12 13:26:26 -0500

    finished LivingConditions

--- Animal_Subsystem/LivingCondition.java
+++ Animal_Subsystem/LivingCondition.java
@@ -1,8 +1,42 @@
 package Animal_Subsystem;
-public class LivingCondition {
-    public LivingCondition() {
+public abstract class LivingCondition {
+    private double temperature;
+    private double humidity;
+    private double area;
+    private String region;
+
+    public LivingCondition(double temp, double hum, double area, String region) {
+        this.temperature = temp;
+        this.humidity = hum;
+        this.area = area;
+        this.region = region;
     }  
-    public double compareTo(LivingCondition other) {
-        return 0;
+    public abstract double compareTo(LivingCondition other);
+
+    // GETTERS
+    public double getTemperature() {
+        return temperature;
+    }
+    public double getHumidity() {
+        return humidity;
+    }
+    public double getArea() {
+        return area;
+    }
+    public String getRegion() {
+        return region;
+    }
+    // SETTERS
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+    public void setHumidity(double humidity) {
+        this.humidity = humidity;
+    }
+    public void setArea(double area) {
+        this.area = area;
+    }
+    public void setRegion(String region) {
+        this.region = region;
     }
 }
